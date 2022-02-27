2afcfcd4950dae8f3c3e7428c98daa9ebbb23237
seubpong.mon <pjame.fb@gmail.com>
2022-02-27 19:39:23 +0700

    Fix reference

--- 00-0-init/sa.tf
+++ 00-0-init/sa.tf
@@ -8,11 +8,11 @@ resource "google_service_account" "wp_service_account" {
 resource "google_project_iam_member" "wp_storage_viewer" {
   project = var.project
   role    = "roles/storage.objectViewer"
-  member  = "serviceAccount:${google_service_account.ipfs_service_account.email}"
+  member  = "serviceAccount:${google_service_account.wp_service_account.email}"
 }
 
 resource "google_project_iam_member" "wp_sm_accessor" {
   project = var.project
   role    = "roles/secretmanager.secretAccessor"
-  member  = "serviceAccount:${google_service_account.ipfs_service_account.email}"
+  member  = "serviceAccount:${google_service_account.wp_service_account.email}"
 }
