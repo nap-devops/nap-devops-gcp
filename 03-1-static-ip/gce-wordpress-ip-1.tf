dc928fb1c0b542d3c1f60ceda029f34a4da43829
seubpong.mon <pjame.fb@gmail.com>
2022-02-27 19:49:38 +0700

    Added static IP

---
+++ 03-1-static-ip/gce-wordpress-ip-1.tf
@@ -0,0 +1,17 @@
+resource "google_compute_global_address" "gce_wp" {
+  project      = var.project
+  name         = "gce-wp-1"
+  ip_version   = "IPV4"
+  address_type = "EXTERNAL"
+}
+
+resource "google_dns_record_set" "gce_wp_dns_1" {
+  project = var.project
+
+  name = "wordpress.${var.top_level_domain}."
+  type = "A"
+  ttl  = 300
+  managed_zone = "devops"
+
+  rrdatas = [google_compute_global_address.gce_wp.address]
+}
