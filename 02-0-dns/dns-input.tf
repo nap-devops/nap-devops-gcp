f5d0f2d04e60797b47852f82e7c88dc88c73bf79
seubpong.mon <pjame.fb@gmail.com>
2022-04-23 12:28:28 +0700

    Added new htts LB

--- 02-0-dns/dns-input.tf
+++ 02-0-dns/dns-input.tf
@@ -3,3 +3,9 @@ variable "top_level_domain" {
   type     = string
   nullable = false 
 }
+
+variable "top_level_domain2" {
+  description = "Custom domain name."
+  type        = string
+  default     = ""
+}
