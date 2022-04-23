963f7bb3a710bbf61329812274641969494945fd
seubpong.mon <pjame.fb@gmail.com>
2022-04-23 11:27:57 +0700

    Added new instance group for Aldamex

--- 05-0-isg-wp/wp-asia-southeast1-group.tf
+++ 05-0-isg-wp/wp-asia-southeast1-group.tf
@@ -9,3 +9,14 @@ module "group-wp-ase1" {
   zone_b_instances = []
   zone_c_instances = []
 }
+
+module "group-wp-adm-ase1" {
+  source  = "../modules/instance-group"
+
+  project = var.project
+  region = "asia-southeast1"
+  type = "wp"
+  zone_a_instances = ["aldamex"]
+  zone_b_instances = []
+  zone_c_instances = []
+}
